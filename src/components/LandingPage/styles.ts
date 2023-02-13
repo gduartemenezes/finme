9a9292a5896cb7bfcc45a032e3eff46cd6e10398
Gustavo Duarte <gduartemenezes@gmail.com>
2023-02-13 16:38:44 -0300

    feat: update styles to follow figma design

--- src/components/LandingPage/styles.ts
+++ src/components/LandingPage/styles.ts
@@ -3,15 +3,17 @@ import styled from 'styled-components'
 export const Container = styled.div`
     height: 100vh;
     width: 100%;
-    background: #000000;
+    background: #212529;
+    color: #FFFFFF;
 `
 export const Content = styled.div`
-    width: 70%;
+    width: 80%;
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
     margin: auto;
+    padding: 200px 0;
     @media(max-width: 768px) {
         width: 90%;
         display: flex;
@@ -22,8 +24,11 @@ export const Content = styled.div`
 `
 
 export const ContentChild = styled.div`
-    width: 45%;
+    width: 50%;
     display: flex;
     flex-direction: column;
     justify-content: ${props => props ? '': ''}  flex-start;
+`
+export const TextDiv = styled.div`
+
 `
