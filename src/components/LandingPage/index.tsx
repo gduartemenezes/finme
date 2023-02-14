c171a5f42f63243d88ebf03689440a61efcff739
Gustavo Duarte <gduartemenezes@gmail.com>
2023-02-14 00:14:40 -0300

    feat: import button component

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,16 +1,19 @@
 import {Container,Content,ContentChild, TextDiv} from './styles'
 import illustrationImg from '../../assets/Illustration.png'
+
+import { Button } from '../UI/Button'
+
 export default function LandingPage() {
     return <Container>
         <Content>
             <ContentChild>
-                <div>FinME</div>
+                <TextDiv>FinME</TextDiv>
                 <TextDiv><p className='title-1'>Centralize o <br/> controle das suas finanças</p></TextDiv>
                 <TextDiv><p className='headline'>de forma rápida e segura</p></TextDiv>
-                <button>Iniciar</button>
+                <Button onClick={()=>{}} text='Iniciar' width='241px' />
             </ContentChild>
             <ContentChild>
-                <img src={illustrationImg} alt="Illustration" />
+                <img src={illustrationImg} alt="Illustration" width={"90%"} height={"90%"} />
             </ContentChild>
         </Content>
     </Container>
